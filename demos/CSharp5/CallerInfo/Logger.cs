31c456e5fbbb484e18a2b654ff8fbc701e711155
miensol <piotr.mionskowski@gmail.com>
2012-05-18 11:41:06 +0200

    added sample with begin end

---
+++ demos/CSharp5/CallerInfo/Logger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallerInfo
+{
+    public class Logger
+    {
+        private string _className;
+
+        private Logger(string mayBeClassName)
+        {
+            _className = mayBeClassName;
+        }
+
+        public static Logger Get([CallerMemberName]string memberName = "")
+        {
+            return new Logger(memberName);
+        }
+
+        public void Log(string message, [CallerMemberName]string memberName = "", [CallerLineNumber]int lineNumber = 0)
+        {
+            Console.WriteLine("[{0}.{1} - {2}] - {3}", _className, memberName, lineNumber, message);            
+        }
+    }
+}
---
+++ demos/CSharp5/DownloadPage.BeginEnd/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using CallerInfo;
+
+namespace DownloadPage.BeginEnd
+{
+    class Program
+    {
+        private static Logger ProgramLog = Logger.Get();
+
+        static void Main(string[] args)
+        {
+            var address = new Uri("http://www.ddgoodgle.com");
+
+            var request = WebRequest.CreateHttp(address);
+            ProgramLog.Log("Created http request");
+            //here another exception may be thrown
+            request.BeginGetResponse(EndGetResposne, request);
+            ProgramLog.Log("Fired BeginGetResponse");
+            
+            ProgramLog.Log("Hit enter to close");
+            Console.ReadLine();
+        }
+
+        private static void EndGetResposne(IAsyncResult ar)
+        {
+            ProgramLog.Log(string.Format("Completed synchrounously {0}", ar.CompletedSynchronously));
+            var request = (HttpWebRequest) ar.AsyncState;
+            try
+            {
+                var response = (HttpWebResponse)request.EndGetResponse(ar);
+                using (var streamReader = new StreamReader(response.GetResponseStream()))
+                {
+                    ProgramLog.Log("Got response stream");
+                    ProgramLog.Log(string.Format("{0} ...",streamReader.ReadToEnd().Substring(0, 100)));
+                }
+            }
+            catch (WebException exception)
+            {
+                ProgramLog.Log("Excepiton occured");
+                ProgramLog.Log(exception.Message);
+            }            
+        }
+    }
+}
