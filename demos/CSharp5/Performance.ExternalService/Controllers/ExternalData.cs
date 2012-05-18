11ae3ac21f6c5bd867b923e9b9111d4e90f201d3
miensol <piotr.mionskowski@gmail.com>
2012-05-18 13:59:32 +0200

    Working external service

---
+++ demos/CSharp5/Performance.ExternalService/Controllers/ExternalData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+
+namespace Performance.ExternalService.Controllers
+{
+    public class ExternalDataController : ApiController
+    {
+         public HttpResponseMessage Get()
+         {
+             Thread.Sleep(TimeSpan.FromMilliseconds(500));
+             return new HttpResponseMessage
+                        {
+                            Content = new StringContent("This is very important data that took more than 500ms to prepare")
+                        };
+         }
+    }
+}
--- demos/CSharp5/Performance.ExternalService/Program.cs
+++ demos/CSharp5/Performance.ExternalService/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http.SelfHost;
+using System.Web.Http;
 
 namespace Performance.ExternalService
 {
@@ -10,6 +12,18 @@ namespace Performance.ExternalService
     {
         static void Main(string[] args)
         {
+            var httpConfig = new HttpSelfHostConfiguration("http://localhost:8001");
+            httpConfig.Routes.MapHttpRoute("Default", "{controller}/{id}", new
+                                                                               {
+                                                                                   id = RouteParameter.Optional
+                                                                               });
+
+            using (var server = new HttpSelfHostServer(httpConfig))
+            {
+                server.OpenAsync().Wait();
+                Console.WriteLine("Listening at " + httpConfig.BaseAddress);
+                Console.ReadLine();
+            }
         }
     }
 }
