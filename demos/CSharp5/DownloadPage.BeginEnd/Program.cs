d0c97736473e0a8f9071d553d8937478ae71332d
miensol <piotr.mionskowski@gmail.com>
2012-05-18 11:54:02 +0200

    added reactive version

--- demos/CSharp5/DownloadPage.BeginEnd/Program.cs
+++ demos/CSharp5/DownloadPage.BeginEnd/Program.cs
@@ -11,39 +11,44 @@ namespace DownloadPage.BeginEnd
 {
     class Program
     {
-        private static Logger ProgramLog = Logger.Get();
+        private static Logger BeginEndProgram = Logger.Get();
 
         static void Main(string[] args)
+        {
+            PerformRequest();
+
+            BeginEndProgram.Log("Hit enter to close");
+            Console.ReadLine();
+        }
+
+        private static void PerformRequest()
         {
             var address = new Uri("http://www.ddgoodgle.com");
 
             var request = WebRequest.CreateHttp(address);
-            ProgramLog.Log("Created http request");
+            BeginEndProgram.Log("Created http request");
             //here another exception may be thrown
             request.BeginGetResponse(EndGetResposne, request);
-            ProgramLog.Log("Fired BeginGetResponse");
-            
-            ProgramLog.Log("Hit enter to close");
-            Console.ReadLine();
+            BeginEndProgram.Log("Fired BeginGetResponse");
         }
 
         private static void EndGetResposne(IAsyncResult ar)
         {
-            ProgramLog.Log(string.Format("Completed synchrounously {0}", ar.CompletedSynchronously));
+            BeginEndProgram.Log(string.Format("Completed synchrounously {0}", ar.CompletedSynchronously));
             var request = (HttpWebRequest) ar.AsyncState;
             try
             {
                 var response = (HttpWebResponse)request.EndGetResponse(ar);
                 using (var streamReader = new StreamReader(response.GetResponseStream()))
                 {
-                    ProgramLog.Log("Got response stream");
-                    ProgramLog.Log(string.Format("{0} ...",streamReader.ReadToEnd().Substring(0, 100)));
+                    BeginEndProgram.Log("Got response stream");
+                    BeginEndProgram.Log(string.Format("{0} ...",streamReader.ReadToEnd().Substring(0, 100)));
                 }
             }
             catch (WebException exception)
             {
-                ProgramLog.Log("Excepiton occured");
-                ProgramLog.Log(exception.Message);
+                BeginEndProgram.Log("Excepiton occured");
+                BeginEndProgram.Log(exception.Message);
             }            
         }
     }
---
+++ demos/CSharp5/DownloadPage.Reactive/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CallerInfo;
+
+namespace DownloadPage.Reactive
+{
+    internal class Program
+    {
+        private static Logger Reactive = Logger.Get();
+
+        private static void Main(string[] args)
+        {
+            PerformRequest();
+
+            Reactive.Log("Hit enter to close");
+            Console.ReadLine();
+        }
+
+        private static void PerformRequest()
+        {
+            var address = new Uri("http://www.ddgoodgle.com");
+
+            var request = WebRequest.CreateHttp(address);
+            Reactive.Log("Created http request");
+            //here another exception may be thrown
+
+            var observable = Observable.FromAsyncPattern(
+                request.BeginGetResponse,
+                result => (HttpWebResponse) request.EndGetResponse(result)
+            );
+
+            observable().Subscribe(ReadResponseStream, OnError);
+
+            Reactive.Log("Fired BeginGetResponse");
+        }
+
+        private static void ReadResponseStream(HttpWebResponse obj)
+        {
+            try
+            {
+                using (var streamReader = new StreamReader(obj.GetResponseStream()))
+                {
+                    Reactive.Log("Received response stream");
+                    Reactive.Log(string.Format("{0}...", streamReader.ReadToEnd().Substring(0,100)));
+                }
+            }
+            catch (ProtocolViolationException ex)
+            {
+                Reactive.Log("Connection was abruptly closed?");
+            }
+        }
+
+        public static void OnError(Exception ex)
+        {
+            Reactive.Log("Exception occured");
+            Reactive.Log(ex.Message);
+        }
+    }
+}
