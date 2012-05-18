cf23c1905018e4ee204211821e1fd47fa3ad844a
miensol <piotr.mionskowski@gmail.com>
2012-05-18 13:17:09 +0200

    Created performance projects

---
+++ demos/CSharp5/Performance.WebApp/Controllers/ExpensiveController.cs
@@ -0,0 +1,55 @@
+﻿using System.Web.Mvc;
+
+namespace Performance.WebApp.Controllers
+{
+    public class ExpensiveController : Controller
+    {
+        private readonly RemoteService _remoteService;
+        private readonly DatabaseQuery _database;
+
+        public ExpensiveController():this(new RemoteService(), new DatabaseQuery())
+        {
+        }
+
+
+        public ExpensiveController(RemoteService remoteService, DatabaseQuery database)
+        {
+            _remoteService = remoteService;
+            _database = database;
+        }
+
+        public ActionResult Execute()
+        {
+            return View(new ExecuteExpensiveViewModel
+                            {
+                                ExternalData = _remoteService.ReadData(),
+                                QueryResult = _database.GetInt("")
+                            });
+        }
+
+
+
+    }
+
+    public class RemoteService
+    {
+        public string ReadData()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class DatabaseQuery
+    {
+        public int GetInt(string sqlQuery)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class ExecuteExpensiveViewModel 
+    {
+        public string ExternalData { get; set; }
+        public int QueryResult { get; set; }
+    }
+}
---
+++ demos/CSharp5/Performance.WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Performance.WebApp.MvcApplication" Language="C#" %>
