679b078461f30259cc84ffb3ef6f73166abb2dcb
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2012-05-23 21:48:44 +0200

    added animation of merging routines

--- demos/CSharp5/DeepDive/FifoCoordinator.cs
+++ demos/CSharp5/DeepDive/FifoCoordinator.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
+using CallerInfo;
+using NUnit.Framework;
 
 namespace DeepDive
 {
@@ -20,6 +22,57 @@ namespace DeepDive
             {
                 _queue.Dequeue().Resume();
             }
+        }
+    }
+
+    public class FifoCoorindatorTest
+    {
+        public static Logger Fifo = Logger.Get();
+
+        [Test]
+        public void should_show_how_coroutines_work()
+        {
+            var coordinator = new FifoCoordinator();
+
+            FirstRoutine(coordinator);
+            SecondRoutine(coordinator);
+            ThirdRooutine(coordinator);
+            
+            coordinator.Flush();
+        }
+
+        private async void FirstRoutine(FifoCoordinator coordinator)
+        {
+            Fifo.Log("Entered First");
+            
+            await coordinator.AtEndOfQueue();
+            Fifo.Log("Back to First");
+            
+            await coordinator.AtEndOfQueue();
+            Fifo.Log("Ending First");
+        }
+
+        private async void SecondRoutine(FifoCoordinator coordinator)
+        {
+            Fifo.Log("Entered Second");
+            
+            await coordinator.AtEndOfQueue();
+            
+            Fifo.Log("Ending Second");
+        }
+
+        private async void ThirdRooutine(FifoCoordinator coordinator)
+        {
+            Fifo.Log("Entered Third");
+            
+            await coordinator.AtEndOfQueue();
+            Fifo.Log("Back to Third");
+            
+            await coordinator.AtEndOfQueue();
+            Fifo.Log("Back again to Third");
+            
+            await coordinator.AtEndOfQueue();
+            Fifo.Log("Ending Third");
         }
     }
 }
